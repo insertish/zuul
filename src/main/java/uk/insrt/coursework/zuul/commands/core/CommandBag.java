9d709729e8240f70d4db7d0d596d176d8eaf122b
Paul <paulmakles@gmail.com>
2021-11-25 18:27:36 +0000

    feat(command): allow looking at other's inventory

--- src/main/java/uk/insrt/coursework/zuul/commands/core/CommandBag.java
+++ src/main/java/uk/insrt/coursework/zuul/commands/core/CommandBag.java
@@ -12,25 +12,46 @@ import uk.insrt.coursework.zuul.world.World;
 
 public class CommandBag extends Command {
     public CommandBag() {
-        super("bag", "look inside your bag",
+        super("inventory [of <something>]", "look inside your bag or at something's inventory",
             new Pattern[] {
-                Pattern.compile("^b(?:ag)*(?!\\w)"),
-                // b, bag
+                Pattern.compile("^(?:b(?:ag)|inv(?:entory))(?:\\s+(?<entity>[\\w\\s]+))*"),
+                // b, bag, inv, inventory, bag of <entity>, inventory of <entity>, (+2)
             });
     }
 
     @Override
     public boolean run(World world, Arguments arguments) {
         IOSystem io = world.getIO();
-        Inventory inv = world.getPlayer().getInventory();
 
+        Entity entity;
+        boolean ours;
+        if (arguments.has("entity")) {
+            ours = false;
+            entity = this.findEntity(world, arguments, "Can't find what you want to look at.");
+            if (entity == null) return false;
+        } else {
+            ours = true;
+            entity = world.getPlayer();
+        }
+        
+        Inventory inv = entity.getInventory();
         if (inv.getWeight() == 0) {
-            io.println("Your bag is empty! You can carry " + inv.getMaxWeight() + " kg.");
+            if (ours) {
+                io.println("Your bag is empty! You can carry " + inv.getMaxWeight() + " kg.");
+            } else {
+                io.println(entity.getHighlightedName() + " doesn't appear to have anything.");
+            }
+
             return false;
         }
 
-        io.println("You are carrying " + inv.getWeight() + " / " + inv.getMaxWeight() + " kg.");
-        io.println("You look in your bag to see:");
+        if (ours) {
+            io.println("You are carrying " + inv.getWeight() + " / " + inv.getMaxWeight() + " kg.");
+            io.println("You look in your bag to see:");
+        } else {
+            io.println(entity.getHighlightedName() + " appears to have:");
+        }
+
         for (Entity item : inv.getItems()) {
             io.println("- " + Ansi.Yellow + item.getWeight() + " kg"
                 + Ansi.Reset + " " + item.describe()
